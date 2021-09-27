b2c8f7198c96d136995f168da13fb759006bfc2f
ranjithgee <ranjithgee968@gmail.com>
2021-09-27 12:21:09 +0530

    changes

--- src/components/sub-content-2.vue
+++ src/components/sub-content-2.vue
@@ -148,6 +148,12 @@
                                             <router-link v-for="item in tables.main_3_7_2" :key="item.id" :to="item.link">{{item.name}}</router-link>
                                         </div>
                                 </div>
+                                <div class="year_category" v-if="cat.path == '5.1.1'" >
+                                        <div class="year_head"><img class="send_icon" src="@/assets/send.svg" alt="">Scholarship and freeship-govt.</div>
+                                        <div class="year_items">
+                                            <router-link v-for="item in tables.main_5_1_1" :key="item.id" :to="item.link">{{item.name}}</router-link>
+                                        </div>
+                                </div>
                                 <div class="year_category" v-if="cat.path == '3.6.2'" >
                                         <div class="year_head"><img class="send_icon" src="@/assets/send.svg" alt=""> Awards and Recognition recieved for extension activities</div>
                                         <div class="year_items">
